3b98ddb58f692dbbc80d22b4431fc661ffa5b243
NikolaenkoKonstantin <62340704+NikolaenkoKonstantin@users.noreply.github.com>
2020-11-08 21:10:18 +0300

    Add files via upload

---
+++ lab_2/include/binary.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <random>
+#include <iostream>
+
+class binary {
+private:
+    char S;
+    int n;
+    bool bA[10];
+public:
+    binary();
+    binary(char c);
+
+    binary operator | (const binary &B) const;
+    binary operator & (const binary &B) const;
+
+    binary & operator = (const binary &B);
+    binary & operator &=(const binary &B);
+    binary & operator |=(const binary &B);
+
+    void show();
+    int power(){return n;}
+};
---
+++ lab_2/include/machWord.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <random>
+#include <iostream>
+
+class machWord {
+private:
+    long mA;
+    int n;
+    char S;
+public:
+    machWord();
+    machWord(char c);
+
+    void show();
+    int power(){return n;}
+
+    machWord operator | (const machWord &B) const;
+    machWord operator & (const machWord &B) const;
+
+    machWord & operator = (const machWord &B);
+    machWord & operator &=(const machWord &B);
+    machWord & operator |=(const machWord &B);
+};
